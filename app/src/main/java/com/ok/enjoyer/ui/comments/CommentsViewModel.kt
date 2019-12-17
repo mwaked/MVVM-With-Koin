d6aeb6fe326ae304e1ffff6304748c53205b3d39
Mahmoud Waked <mwaked301@gmail.com>
2019-12-17 14:32:40 +0300

    handle states

--- app/src/main/java/com/ok/enjoyer/ui/comments/CommentsViewModel.kt
+++ app/src/main/java/com/ok/enjoyer/ui/comments/CommentsViewModel.kt
@@ -9,7 +9,6 @@ import kotlinx.coroutines.launch
 import timber.log.Timber
 import kotlin.coroutines.CoroutineContext
 
-
 class CommentsViewModel(private val dataManager: DataManager, uiContext: CoroutineContext = Dispatchers.Main) :
     CouroutineViewModel(uiContext) {
 
